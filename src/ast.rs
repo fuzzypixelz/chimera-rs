12196865e0d5451c8dd210cf7746027a38600534
fuzzypixelz <mazouz.mahmoud@outlook.com>
2022-01-25 02:38:54 +0100

    add attributes to items

--- src/ast.rs
+++ src/ast.rs
@@ -1,18 +1,28 @@
+/// Chimera's abstract syntax tree.
+/// The layout is highly inspired by rustc's own ast.
 use polytype::TypeSchema;
 
 #[derive(Debug, PartialEq, Clone)]
-pub enum Item {
+pub struct Attr {
+    pub name: String,
+    pub args: Vec<String>,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Item {
+    pub attr: Option<Attr>,
+    pub kind: ItemKind,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum ItemKind {
     Definition {
         name: String,
         ann: Option<TypeSchema>,
         expr: Expr,
     },
-    Attribute {
-        name: String,
-        args: Vec<Expr>,
-    },
     DataType {
-        kind: TypeSchema,
+        schema: TypeSchema,
         variants: Vec<(String, Vec<(String, TypeSchema)>)>,
     },
     Module {
@@ -24,32 +34,38 @@ pub enum Item {
 #[derive(Debug, PartialEq, Clone)]
 pub enum Stmt {
     Item(Item),
-    // This is meant to force evaluation of a certain Expr,
-    // for example for the result at a function's end. Or,
-    // for "applying" a function purely for its side-effects.
     Expr(Expr),
 }
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Expr {
     // Primitives
-    Void,     // ()
-    Int(i64), // 42
+    Ellipsis,
+    Void,
+    // ()
+    Int(i64),
+    // 42
     // U64(u64),
     // F64(f64),
-    Bool(bool),  // true / false
-    Char(char),  // 'c'
-    Str(String), // "Hello, World"
+    Bool(bool),
+    // true / false
+    Char(char),
+    // 'c'
     // Functions
-    Name(String), // coolName
+    Name(String),
+    List(Vec<Expr>),
+    // coolName
     // TODO: enforce the fact that the last Instr in a code-block
     // should be an Expr by baking it into the parser, producing
     // a seperate `expr` field here. This will simplify the
     // type-checker somewhat.
-    Lambda { param: String, expr: Box<Expr> }, // |x| x + 1
+    Lambda { param: String, expr: Box<Expr> },
+    // |x| x + 1
     Block { body: Vec<Stmt> },
-    Apply { left: Box<Expr>, right: Box<Expr> }, // f x
-    Branch { paths: Vec<(Expr, Vec<Stmt>)> },    // if cond then 0 else 42 end
+    Apply { left: Box<Expr>, right: Box<Expr> },
+    // f x
+    Branch { paths: Vec<(Expr, Vec<Stmt>)> },
+    // if cond then 0 else 42 end
     Field { expr: Box<Expr>, name: String },
     Assign { left: Box<Expr>, right: Box<Expr> }, // i = 0
 }

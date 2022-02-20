f1f51ddd39c0215c7ebfb86499eed07b2e0dcfc4
fuzzypixelz <mazouz.mahmoud@outlook.com>
2022-02-20 06:53:32 +0100

    [MLIR] add context and module wrappers.

--- src/mlir/mod.rs
+++ src/mlir/mod.rs
@@ -7,7 +7,16 @@ pub mod raw {
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
 
-use raw::MlirStringRef;
+pub mod attribute;
+pub mod block;
+pub mod operation;
+pub mod region;
+pub mod value;
+
+use std::mem::ManuallyDrop;
+
+use operation::Operation;
+use raw::*;
 
 impl From<&str> for MlirStringRef {
     fn from(item: &str) -> Self {
@@ -18,9 +27,82 @@ impl From<&str> for MlirStringRef {
     }
 }
 
-pub mod attribute;
-pub mod block;
-pub mod context;
-pub mod operation;
-pub mod region;
-pub mod value;
+/// Wrapper around the C API's MlirContext since we can't implement Drop for Copy types.
+pub struct Context {
+    context: MlirContext,
+}
+
+impl Context {
+    /// Make an empty MLIR context.
+    ///
+    /// Currently, this also registers all dialects and all passes for your convenience; which is not particularly efficient and is subject to change.
+    pub fn new() -> Self {
+        unsafe {
+            let context = mlirContextCreate();
+            mlirRegisterAllDialects(context);
+            mlirRegisterAllPasses();
+            Context { context }
+        }
+    }
+
+    /// Get a Context from a raw MlirContext.
+    pub fn from_raw(context: MlirContext) -> Self {
+        Context { context }
+    }
+
+    /// Return the underlying raw MlirAttribute.
+    pub fn as_raw(&self) -> MlirContext {
+        self.context
+    }
+}
+
+impl Drop for Context {
+    fn drop(&mut self) {
+        unsafe { mlirContextDestroy(self.context) }
+    }
+}
+
+/// Wrapper around the C API's MlirModule since we can't implement Drop for Copy types.
+pub struct Module {
+    module: MlirModule,
+}
+
+impl Module {
+    /// Make an empty MLIR Module from a source location.
+    pub fn new(location: MlirLocation) -> Self {
+        Module {
+            module: unsafe { mlirModuleCreateEmpty(location) },
+        }
+    }
+
+    /// Append an `operation` to the module's only body block.
+    ///
+    /// We make the opinionated choice of only exposing the block
+    /// this way for now.
+    pub fn append(&mut self, operation: Operation) {
+        unsafe {
+            mlirBlockAppendOwnedOperation(mlirModuleGetBody(self.module), operation.into_raw())
+        }
+    }
+
+    /// Get a Context from a raw MlirModule.
+    pub fn from_raw(module: MlirModule) -> Self {
+        Module { module }
+    }
+
+    /// Return the underlying raw MlirModule.
+    pub fn as_raw(&self) -> MlirModule {
+        self.module
+    }
+
+    /// Return the underlying raw MlirModule and consume the Module.
+    pub fn into_raw(self) -> MlirModule {
+        ManuallyDrop::new(self).module
+    }
+}
+
+impl Drop for Module {
+    fn drop(&mut self) {
+        unsafe { mlirModuleDestroy(self.module) }
+    }
+}
